51cc30a60473e751007e30c055015e327706c99a
Elma <ekupusovic1@etf.unsa.ba>
2022-11-07 12:33:48 +0100

    pocetak

---
+++ src/main/java/ba/unsa/etf/rpr/Laptop.java
@@ -0,0 +1,83 @@
+package ba.unsa.etf.rpr;
+
+
+public class Laptop {
+    String brend, model, procesor, grafickaKartica;
+    int ram, hdd, ssd;
+    double cijena, velicinaEkrana;
+
+    public Laptop() {
+    }
+
+    public String getBrend() {
+        return brend;
+    }
+
+    public void setBrend(String brend) {
+        this.brend = brend;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getProcesor() {
+        return procesor;
+    }
+
+    public void setProcesor(String procesor) {
+        this.procesor = procesor;
+    }
+
+    public String getGrafickaKartica() {
+        return grafickaKartica;
+    }
+
+    public void setGrafickaKartica(String grafickaKartica) {
+        this.grafickaKartica = grafickaKartica;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(int ssd) {
+        this.ssd = ssd;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public double getVelicinaEkrana() {
+        return velicinaEkrana;
+    }
+
+    public void setVelicinaEkrana(double velicinaEkrana) {
+        this.velicinaEkrana = velicinaEkrana;
+    }
+}
